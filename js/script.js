04fef265f1e81d944f4fb7cc54d8c6069c6ad5cd
Victor <vict02r05@gmail.com>
2023-03-29 19:44:52 +0300

    add dots to slider

--- js/script.js
+++ js/script.js
@@ -335,6 +335,9 @@ window.addEventListener('DOMContentLoaded', () => {
     } else {
       current.textContent = slideIndex;
     }
+
+    dots.forEach(dot => (dot.style.opacity = '0.5'));
+    dots[slideIndex - 1].style.opacity = 1;
   });
 
   prev.addEventListener('click', () => {
@@ -356,5 +359,49 @@ window.addEventListener('DOMContentLoaded', () => {
     } else {
       current.textContent = slideIndex;
     }
+
+    dots.forEach(dot => (dot.style.opacity = '0.5'));
+    dots[slideIndex - 1].style.opacity = 1;
+  });
+
+  // Slider Dots
+
+  const slider = document.querySelector('.offer__slider');
+
+  slider.style.position = 'relative';
+
+  const indicators = document.createElement('ol'),
+    dots = [];
+  indicators.classList.add('carousel-indicators');
+  slider.append(indicators);
+
+  for (let i = 0; i < slides.length; i++) {
+    const dot = document.createElement('li');
+    dot.setAttribute('data-slide-to', i + 1);
+    dot.classList.add('dot');
+    if (i == 0) {
+      dot.style.opacity = 1;
+    }
+    indicators.append(dot);
+    dots.push(dot);
+  }
+
+  dots.forEach(dot => {
+    dot.addEventListener('click', e => {
+      const slideTo = e.target.getAttribute('data-slide-to');
+
+      slideIndex = slideTo;
+      offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+      slidesField.style.transform = `translateX(-${offset}px)`;
+
+      if (slides.length < 10) {
+        current.textContent = '0' + slideIndex;
+      } else {
+        current.textContent = slideIndex;
+      }
+
+      dots.forEach(dot => (dot.style.opacity = '0.5'));
+      dots[slideIndex - 1].style.opacity = 1;
+    });
   });
 });
