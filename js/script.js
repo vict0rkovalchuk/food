60ff8057895672d5a695296d057e53e8d482f708
Victor <vict02r05@gmail.com>
2023-03-29 18:39:53 +0300

    add slider; way 1

--- js/script.js
+++ js/script.js
@@ -283,4 +283,50 @@ window.addEventListener('DOMContentLoaded', () => {
       closeModal();
     }, 4000);
   }
+
+  // Slider
+
+  const slides = document.querySelectorAll('.offer__slide'),
+    prev = document.querySelector('.offer__slider-prev'),
+    next = document.querySelector('.offer__slider-next'),
+    total = document.querySelector('#total'),
+    current = document.querySelector('#current');
+  let slideIndex = 1;
+
+  showSlides(slideIndex);
+  if (slides.length < 10) {
+    total.textContent = '0' + slides.length;
+  } else {
+    total.textContent = slides.length;
+  }
+
+  function showSlides(n) {
+    if (n > slides.length) {
+      slideIndex = 1;
+    }
+
+    if (n < 1) {
+      slideIndex = slides.length;
+    }
+
+    if (slides.length < 10) {
+      current.textContent = '0' + slideIndex;
+    } else {
+      current.textContent = slideIndex;
+    }
+
+    slides.forEach(slide => (slide.style.display = 'none'));
+    slides[slideIndex - 1].style.display = 'block';
+  }
+
+  function plusSlides(n) {
+    showSlides((slideIndex += n));
+  }
+
+  prev.addEventListener('click', () => {
+    plusSlides(-1);
+  });
+  next.addEventListener('click', () => {
+    plusSlides(1);
+  });
 });
